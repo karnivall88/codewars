5a98bf212af4d0c44989ed4cec9b115c9087513b
Albert Apsit <albert.apsit@ericsson.com>
2019-12-13 15:41:14 +0200

    Add project files.

---
+++ CodeWars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CodeWars._8kyu;
+using CodeWars._7kyu;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MonkeyCounter.MonkeyCount(5));
+            Console.WriteLine(_7kyu.Kata.rowSumOddNumbers(5));
+        }
+    }
+}
